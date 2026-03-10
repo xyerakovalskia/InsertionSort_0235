0abc86a267efda457145e06e8f21fed8b13f9979
xyerakovalskia <xyera123987@gmail.com>
2026-03-10 10:45:12 +0700

    bagian 4

--- kodingan_1.cpp
+++ kodingan_1.cpp
@@ -26,3 +26,25 @@ void input () { //procedure input
         cin >> arr[i];
     }
 }
+
+void insertionsort() { // procedure insertion sort
+    int temp;
+    int j;
+
+    for (int i = 1; i < n; i++) { // dimulai dari data ke-2 (indeks 1)
+        temp = arr[i]; // simpan nilai yang akan dibandingkan
+        j = i - 1;     // mulai bandingkan dengan data di sebelah kirinya
+
+        while (j >= 0 && arr[j] > temp) { // selama data di kiri lebih besar
+            arr[j + 1] = arr[j];          // geser data ke kanan
+            j--;
+        }
+        arr[j + 1] = temp; // tempatkan nilai temp di posisi yang benar
+
+        // Menampilkan langkah-langkah pengurutan (step-by-step)
+        cout << "\nStep " << i << ": ";
+        for (int k = 0; k < n; k++) {
+            cout << arr[k] << " ";
+        }
+    }
+}
