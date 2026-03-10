b6140f56ed23adf0eb39466c6c2e44b0a883392b
xyerakovalskia <xyera123987@gmail.com>
2026-03-10 10:43:29 +0700

    bagian 2

--- kodingan_1.cpp
+++ kodingan_1.cpp
@@ -3,3 +3,16 @@ using namespace std;
 
 int arr[20]; //membuat array dengan panjang data 20
 int n; //membuat variable inputan n
+
+void input () { //procedure input
+    while (true){
+        cout << "Masukan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArray Tidak Boleh lebih dari 20\n";
+        }
+    }
